e3eb11f3d44d80be6f883c183552ea3cff92fedf
fgrassovillalba <fgrasso15@gmail.com>
2022-07-06 00:32:32 -0300

    Initial commit

---
+++ main.js/main.js
@@ -0,0 +1,15 @@
+console.log('buenas')
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+  ScrollReveal().reveal('.showcase');
+  ScrollReveal().reveal('.news-card', { delay: 500 });
+  ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+  ScrollReveal().reveal('.news-card-DOS', { delay: 500 });
+  ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+  ScrollReveal().reveal('.social', { delay: 500 });
+  ScrollReveal().reveal('.footer-links', { delay: 500 });
+
+
+  
